0394fa2766d9d3f62f411c131af21d7887ffb3f2
rsarai <rebecasaraiaguiar@gmail.com>
2022-01-11 20:46:33 -0300

    Fix deploy

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
+import os
 
 import dash
 import plotly.graph_objects as go
@@ -27,6 +28,8 @@ from constants import (
 )
 
 app = dash.Dash(__name__)
+
+server = app.server
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
